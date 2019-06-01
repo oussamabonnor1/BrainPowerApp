b52bd15ef36c42f061e21a2f65476ebfb957fde2
oussamabonnor1 <oussamabonnor@gmail.com>
2019-06-01 02:06:21 +0100

    adding grid view for the patter buttons, adding UI colors

--- BrainPowerApp/BrainPowerApp/MainPage.xaml.cs
+++ BrainPowerApp/BrainPowerApp/MainPage.xaml.cs
@@ -9,6 +9,7 @@ namespace BrainPowerApp
 {
     public partial class MainPage : ContentPage
     {
+        string[] colors = { "#59DAB4", "#E74C70", "#F188E8", "#5AB1E6" };
         public MainPage()
         {
             InitializeComponent();
