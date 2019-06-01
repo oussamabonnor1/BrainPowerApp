8de278b1fd67707d436795e942087ab697b1d91a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-06-01 23:18:21 +0100

    fully working prototype: sequence display and player input

--- BrainPowerApp/BrainPowerApp/MainPage.xaml.cs
+++ BrainPowerApp/BrainPowerApp/MainPage.xaml.cs
@@ -10,12 +10,17 @@ namespace BrainPowerApp
         string originalButtonColor = "#8D81AA";
         string[] colors = { "#59DAB4", "#E74C70", "#F188E8", "#5AB1E6" };
         List<int> patternIndexes;
-        int times = 0;
+        List<Color> patternColors;
+        bool patternShowing;
+        int currentPatternIndex;
 
         public MainPage()
         {
             InitializeComponent();
             patternIndexes = new List<int>();
+            patternColors = new List<Color>();
+            patternShowing = false;
+            currentPatternIndex = 0;
         }
 
         public void SelectingButton(int buttonIndex, Color color)
@@ -54,14 +59,18 @@ namespace BrainPowerApp
 
         async Task MakingPattern()
         {
+            patternShowing = true;
             patternIndexes.Add(new Random().Next(9));
+            patternColors.Add(ConvertHexaToColor(colors[new Random().Next(colors.Length)]));
             for (int i = 0; i < patternIndexes.Count; i++)
             {
-                SelectingButton(patternIndexes[i], ConvertHexaToColor(colors[new Random().Next(colors.Length)]));
+                SelectingButton(patternIndexes[i], patternColors[i]);
                 //waiting for a bit here
                 await Task.Delay(1000);
                 SelectingButton(patternIndexes[i], ConvertHexaToColor(originalButtonColor));
+                await Task.Delay(250);
             }
+            patternShowing = false;
         }
 
         Color ConvertHexaToColor(string hexaColor)
@@ -73,12 +82,76 @@ namespace BrainPowerApp
 
         public void ButtonClicked(Object sender, EventArgs e)
         {
-
+            var name = ((Button)sender).Id;
+            Console.WriteLine(name);
+            if (name == one.Id)
+            {
+                CheckingPattern(0);
+            }
+            else if (name == two.Id)
+            {
+                CheckingPattern(1);
+            }
+            else if (name == three.Id)
+            {
+                CheckingPattern(2);
+            }
+            else if (name == four.Id)
+            {
+                CheckingPattern(3);
+            }
+            else if (name == five.Id)
+            {
+                CheckingPattern(4);
+            }
+            else if (name == six.Id)
+            {
+                CheckingPattern(5);
+            }
+            else if (name == seven.Id)
+            {
+                CheckingPattern(6);
+            }
+            else if (name == eight.Id)
+            {
+                CheckingPattern(7);
+            }
+            else if (name == nine.Id)
+            {
+                CheckingPattern(8);
+            }
         }
 
-        private void StartGameButtonClicked(object sender, EventArgs e)
+        async void CheckingPattern(int index)
         {
-            MakingPattern();
+            Console.WriteLine(index);
+            if (index == patternIndexes[currentPatternIndex])
+            {
+                //if it s correct, we need to light up the button
+                SelectingButton(patternIndexes[currentPatternIndex], patternColors[currentPatternIndex]);
+                //waiting for a bit here
+                await Task.Delay(500);
+                SelectingButton(patternIndexes[currentPatternIndex], ConvertHexaToColor(originalButtonColor));
+                await Task.Delay(250);
+                //going to the next iteration or waiting..
+                label.Text = "Correct, " + (currentPatternIndex + 1);
+                currentPatternIndex++;
+                if (currentPatternIndex >= patternIndexes.Count)
+                {
+                    currentPatternIndex = 0;
+                    label.Text = "Remember the sequence!";
+                    await MakingPattern();
+                }
+            }
+            else label.Text = "Wrong!";
+        }
+
+        private async void StartGameButtonClicked(object sender, EventArgs e)
+        {
+            if (!patternShowing)
+            {
+                await MakingPattern();
+            }
         }
     }
 }
