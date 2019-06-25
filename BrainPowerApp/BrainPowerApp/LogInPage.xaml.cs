c2eae292483ebbaade97acd158e433c08cb20476
oussamabonnor1 <oussamabonnor@gmail.com>
2019-06-25 01:23:36 +0100

    adding avatar selection ui and resources adding custom font icons

--- BrainPowerApp/BrainPowerApp/LogInPage.xaml.cs
+++ BrainPowerApp/BrainPowerApp/LogInPage.xaml.cs
@@ -7,14 +7,27 @@ namespace BrainPowerApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LogInPage : ContentPage
 	{
-		public LogInPage ()
+        public string[] iconPaths = { "profile.png", "blueplayer.png", "redplayer.png" };
+        int iconIndex;
+        public LogInPage ()
 		{
 			InitializeComponent ();
-		}
+            iconIndex = 0;
+            avatar.Source = iconPaths[iconIndex];
+        }
 
         private void RegisterPlayer(object sender, System.EventArgs e)
         {
 
+        }
+
+        private void ChangeAvatar(object sender, System.EventArgs e)
+        {
+            ImageButton button = (ImageButton)sender;
+            iconIndex = button.Id == leftArraow.Id ? iconIndex - 1 : iconIndex + 1;
+            if (iconIndex < 0) iconIndex = iconPaths.Length - 1;
+            iconIndex = iconIndex % iconPaths.Length;
+            avatar.Source = iconPaths[iconIndex];
         }
     }
 }
